ca43fc9c5bcaa4e6791bd503e2fd4332564609b7
andrespalma02 <69227381+andrespalma02@users.noreply.github.com>
2020-09-09 23:58:28 -0500

    Version corregida

--- puzzle.py
+++ puzzle.py
@@ -33,6 +33,13 @@ class Posicion:
     def setY(self, y):
         self.y = y
 
+    def obtenerCoordenadas(self):
+        return self.x, self.y
+
+    def actualizarCoordenadas(self, x, y):
+        self.x = x
+        self.y = y
+
 
 class Cuadro(ABC):
     def __init__(self, posicionRef, posicionAct):
@@ -59,9 +66,9 @@ class Cuadro(ABC):
 
 class CuadroVacio(Cuadro):
 
-    def __init__(self, posicionR, imagen, posicionA):
+    def __init__(self, posicionR, imagen):
         self._Cuadro__posicionReferencial = posicionR
-        self._Cuadro__posicionActual = posicionA
+        self._Cuadro__posicionActual = None
         self.imagen = pygame.image.load(imagen)
 
     def dibujar(self, fondo):
@@ -92,13 +99,21 @@ class CuadroVacio(Cuadro):
         y = self.posicionActual.getY()
         return x, y
 
+    def getPosicionRef(self):
+        x = self.posicionReferencial.getX()
+        y = self.posicionReferencial.getY()
+        return x, y
+
+    def setPosicionActual(self, posicion):
+        self._Cuadro__posicionActual = Posicion(posicion[0], posicion[1])
+
 
 class FragmentoImagen(Cuadro):
 
-    def __init__(self, posicionR, imagen, posicionA):
+    def __init__(self, posicionR, imagen):
         self._Cuadro__posicionReferencial = posicionR
+        self._Cuadro__posicionActual = None
         self.imagen = imagen
-        self._Cuadro__posicionActual = posicionA
         self.estaOculto = False
 
     def dibujar(self, fondo):
@@ -115,152 +130,135 @@ class FragmentoImagen(Cuadro):
         y = self.posicionActual.getY()
         return x, y
 
-    def setEstado(self, estadoOculto):
-        self.estaOculto = estadoOculto
+    def getPosicionRef(self):
+        x = self.posicionReferencial.getX()
+        y = self.posicionReferencial.getY()
+        return x, y
 
-    def setImagen(self, imagen):
-        self.imagen = pygame.image.load(imagen)
+    def setPosicionActual(self, posicion):
+        self._Cuadro__posicionActual = Posicion(posicion[0], posicion[1])
+
+    def ocultar(self):
+        self.estaOculto = True
+        self.imagen = pygame.image.load("CuadroVacio.png")
 
     def verificarOcultamiento(self):
         return self.estaOculto
 
 
 class Imagen(Cuadro):
-    def __init__(self):
+    def __init__(self,contador,verificacion):
         self._Cuadro__posicion = None
         self.imagen = None
         self.dimension = DIM
         self.lista_cuadros = list()
-        self.cuadro_vacio = None
+        self.verificacion=verificacion
+        self.contador=contador
 
     def mover(self, colision):
-        self.cuadro_vacio.iniciarMovimiento(colision)
+        self.lista_cuadros[-1].iniciarMovimiento(colision)
 
     def dibujar(self, posicion, imagen, pantalla):
         self.imagen = pygame.image.load(imagen)
         self._Cuadro__posicion = posicion
 
-
     def descomponer(self):
-        listaimg = list()
-        listapos = list()
         listarand = list()
-
-        for i in range(N):
-            for j in range(N):
-                posx = int(40 + j * DIM)
-                posy = int(40 + i * DIM)
-                listapos.append((posx, posy))  # Se guardan las posiciones correctas de los fragmento
-                listarand.append((posx, posy))
-                imaux = pygame.Surface((DIM, DIM))  # Se crea una superficie
-                imaux.blit(self.imagen, (0, 0), (posx - 40, posy - 40, DIM, DIM))
-                listaimg.append(imaux)
+        for item in self.lista_cuadros:
+            listarand.append(item.getPosicionRef())
         random.shuffle(listarand)
-        for i in range(len(listapos) - 1):
-            self.agregarCuadro(FragmentoImagen(
-                Posicion(listapos[i][0], listapos[i][1]),
-                listaimg[i],
-                Posicion(listarand[i][0], listarand[i][1])))
-
-        self.cuadro_vacio = \
-            CuadroVacio(Posicion(listapos[len(listapos) - 1][0], listapos[len(listapos) - 1][1]),
-                        "CuadroVacio.png",
-                        Posicion(listarand[len(listapos) - 1][0], listarand[len(listapos) - 1][1]))
+        i = int(0)
+        for item in self.lista_cuadros:
+            item.setPosicionActual(listarand[i])
+            i += 1
 
     def actualizarImagen(self, ventana):
         for i in range(len(self.lista_cuadros)):
             self.lista_cuadros[i].dibujar(ventana)
-        self.cuadro_vacio.dibujar(ventana)
         ventana.blit(self.imagen, (500, 40))
 
     def agregarCuadro(self, fragmentoimagen):
         self.lista_cuadros.append(fragmentoimagen)
 
     def intercambiar(self, posicion):
+
+        listapos = list()
         for elemento in self.lista_cuadros:
+            listapos.append((elemento.getPosicionActual(),elemento.getPosicionRef()))
             if (elemento.getPosicionActual() == posicion):
-                elemento.mover(self.cuadro_vacio.getPosicionActual())
-        self.cuadro_vacio.mover(posicion)
-
-    def ocultar(self):
-        '''
-        lispos = list()
-        for i in range(len(self.lista_cuadros)):
-            lispos.append(i)
-        random.shuffle(lispos)
-        for i in lispos:
-            if (self.lista_cuadros[i].verificarOcultamiento()):
-                pass
-            else:
-                self.lista_cuadros[i].setEstado(True)
-                self.lista_cuadros[i].setImagen("CuadroVacio.png")
-                break
-        :return:
-        '''
-        pass
-
-    def notificar(self):
-        pass
-
-    def getLista(self):
-        return self.lista_cuadros
-
+                self.contador.aumentar()
+                elemento.mover(self.lista_cuadros[-1].getPosicionActual())
+                self.contador.verificar(elemento)
+        self.lista_cuadros[-1].mover(posicion)
+        self.verificacion.verificarCondiciones(self.lista_cuadros)
 
 class Contador:
-    def __init__(self, imagen, verificacion):
+    def __init__(self):
         self.numeroMovimientos = int(0)
-        self.imagen = imagen
-        self.verificacion = verificacion
 
     def aumentar(self):
         self.numeroMovimientos += 1
-        self.verificacion.verificarCondiciones(self.numeroMovimientos)
-        self.verificar()
+        print(self.numeroMovimientos)
 
-    def verificar(self):
-        if (self.numeroMovimientos % 3 == 0) & (self.numeroMovimientos >= 3):
-            self.imagen.ocultar()
-
+    def verificar(self, fragmento):
+        if (self.numeroMovimientos % 5 == 0) & (self.numeroMovimientos >= 5) & isinstance(fragmento,FragmentoImagen):
+            fragmento.ocultar()
 
 class Colision:
-    def __init__(self, contador, imagen):
+    def __init__(self, imagen):
         self.imagen = imagen
-        self.contador = contador
 
     def verificarColision(self, posicion):
 
-        for pos in self.imagen.getLista():
+        for pos in self.imagen.lista_cuadros:
             if pos.getPosicionActual() == posicion:
                 self.imagen.intercambiar(posicion)
-                self.contador.aumentar()
+                break
+
 
 
 class Verificacion:
-    def __init__(self, imagen, puntaje):
-        self.imagen = imagen
+    def __init__(self, puntaje):
         self.puntaje = puntaje
 
-    def verificarCondiciones(self, numeroMovimientos):
-        cont = 0
+    def verificarCondiciones(self, listafragmentos):
+        contG = 0
+        contP=0
+        for elemento in listafragmentos:
+            if isinstance(elemento,FragmentoImagen):
+                if elemento.verificarOcultamiento():
+                    contP += 1
+        if contP == len(listafragmentos)-1:
+            self.puntaje.calcularPuntaje(False)
 
-        for elemento in self.imagen.getLista():
-            if ((elemento.posicionReferencial.getX(), elemento.posicionReferencial.getY())
-                    == (elemento.posicionActual.getX(), elemento.posicionActual.getY())):
-                cont += 1
-        if cont == len(self.imagen.getLista()):
-            self.puntaje.calcularPuntaje(numeroMovimientos)
+        for elemento in listafragmentos:
+            if (elemento.getPosicionActual()==elemento.getPosicionRef()):
+                contG += 1
+
+        if contG == len(listafragmentos):
+            self.puntaje.calcularPuntaje(True)
 
 
 class Puntaje:
 
-    def __init__(self, puzzle):
-        self.puzzle = puzzle
+    def __init__(self,puzzle, contador):
+        self.contador = contador
+        self.puzzle=puzzle
         self.puntajeFinal = 0
 
-    def calcularPuntaje(self, numeroMovimientos):
-        self.puntajeFinal = int(100000 / numeroMovimientos)
-        print(self.puntajeFinal)
+    def calcularPuntaje(self,estado):
+        if estado:
+            self.puntajeFinal = int(100000 / self.contador.numeroMovimientos)
+            print(self.puntajeFinal)
+            return self.puntajeFinal
+        else:
+            print(self.puntajeFinal)
+            return self.puntajeFinal
+    def getpuntaje(self):
+        return self.puntajeFinal
 
+class Mensaje:
+    pass
 
 # ponle tipo juego en el constructor
 class Puzzle():
@@ -270,15 +268,46 @@ class Puzzle():
         pantalla_juego = pygame.display.set_mode(DIMENSION)  # Se crea la ventana con las dimensiones especificas
         titulo_juego = pygame.display.set_caption('I <3 PUZZLE')  # Se inserta un titulo a la ventana creada
         pygame.init()
-        clock = pygame.time.Clock()
-        imagen = Imagen()
+
+        # Instancia de Contador
+        contador = Contador()
+        # Instancia de Puntaje
+        puntaje = Puntaje(self, contador)
+        # instancia de la imagen
+        imagen = Imagen(contador, Verificacion(puntaje))
+        # Instancia de Verificacion
+        # Instancia de Colision
+        colision = Colision(imagen)
         imagen.dibujar(Posicion(0, 0), "ImagenMonitor.png", pantalla_juego)
+
+        # Instancias de los fragmentos
+        listapos = list()
+        listaimg = list()
+        listaFragmentos = list()
+        for i in range(N):
+            for j in range(N):
+                posx = int(40 + j * DIM)
+                posy = int(40 + i * DIM)
+                listapos.append((posx, posy))  # Se guardan las posiciones correctas de los fragmento
+                imaux = pygame.Surface((DIM, DIM))  # Se crea una superficie
+                imaux.blit(pygame.image.load("ImagenMonitor.png"), (0, 0), (posx - 40, posy - 40, DIM, DIM))
+                listaimg.append(imaux)
+
+        #Se agrega las listas de imagenes y las posiciones de referencia a la lista de Fragmentos
+        for i in range(len(listapos)):
+             listaFragmentos.append(FragmentoImagen(
+                    Posicion(listapos[i][0], listapos[i][1]),
+                    listaimg[i]))
+
+        # Instancia del cuadro Vacio
+        cuadro_vacio = CuadroVacio(Posicion(listapos[-1][0], listapos[-1][1]), "CuadroVacio.png")
+
+        for i in range(len(listaFragmentos) - 1):
+            imagen.agregarCuadro(listaFragmentos[i])
+
+        imagen.agregarCuadro(cuadro_vacio)
         imagen.descomponer()
-        puzzle = Puzzle()
-        puntaje = Puntaje(puzzle)
-        verificacion = Verificacion(imagen, puntaje)
-        contador = Contador(imagen, verificacion)
-        colision = Colision(contador, imagen)
+        clock = pygame.time.Clock()
 
         iniciado = True
         while iniciado:
@@ -290,12 +319,6 @@ class Puzzle():
                 if event.type == pygame.QUIT:
                     iniciado = False
                     pygame.quit()
-                if puntaje.puntajeFinal != 0:
-                    ctypes.windll.user32.MessageBoxW(0, "TU PUNTAJE OBTENIDO FUE:" + str(puntaje.puntajeFinal)
-                                                     , "FELICIDADES GANASTE!!", 1)
-                    iniciado = False
-                    pygame.quit()
-                    puzzle.finalizarJuego()
 
 
     def finalizarJuego(self):
